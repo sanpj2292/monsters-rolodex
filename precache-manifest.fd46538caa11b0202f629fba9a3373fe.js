f97168f557e2a9e3b086df2940c17948b1fd8bb1
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-14 18:31:25 +0530

    Updates

---
+++ precache-manifest.fd46538caa11b0202f629fba9a3373fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702c52fb7c25872066eaabb4fa869a10",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9d0cc3482ccbf106af3e",
+    "url": "/monsters-rolodex/static/css/main.7d7394f3.chunk.css"
+  },
+  {
+    "revision": "0d0ff1038fb1e7a453a9",
+    "url": "/monsters-rolodex/static/js/2.4aaf030a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.4aaf030a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0cc3482ccbf106af3e",
+    "url": "/monsters-rolodex/static/js/main.49e7f487.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
