2ffe92e3080afdbb075bc214b124d96c8cc8a498
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-14 18:26:13 +0530

    First complete app finish commit using React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list';
+import { SearchBox } from './components/search-box/search-box';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      search: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(res => this.setState({ monsters: res }))
+      .catch(e => console.error(e));
+  }
+
+  handleClick = () => {
+    console.log(this);
+    alert('I clicked!');
+  }
+
+  onSearch = (e) => {
+    this.setState({ 'search': e.target.value });
+  }
+
+  render() {
+    const { monsters, search } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(search.toLowerCase()));
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='Search for Monsters'
+          handleChange={this.onSearch} />
+        <CardList monsters={filteredMonsters} />
+        <button onClick={this.handleClick}>Click Me!</button>
+      </div>
+
+    );
+  }
 }
 
 export default App;
