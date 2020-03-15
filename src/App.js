01f42035d9d9334f9076ad30f430e129c61cb48f
Sai Sankeerth <sankeerth2292@gmail.com>
2020-03-15 07:25:42 +0530

    Added comments to help understand React in a better manner

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import { CardList } from './components/card-list/card-list';
 import { SearchBox } from './components/search-box/search-box';
 
 class App extends React.Component {
+  // constructor(props) {
+  //  super(props);....
+  // }--> This is a good practice
   constructor() {
     super();
     this.state = {
       monsters: [],
-      search: ''
+      search: '',
+      count: 1
     };
   }
+  componentDidCatch
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -23,12 +28,23 @@ class App extends React.Component {
     this.setState({ 'search': e.target.value });
   }
 
+  handleClick = (e) => {
+    // This sort of syntax is to be used when calculation(s) are made
+    // using the formerState & update it to the current state
+    // Similarly for prevProps as well
+    this.setState((prevState, prevProps) => {
+      return { count: prevState.count + 1 }
+    });
+  };
+
   render() {
-    const { monsters, search } = this.state;
+    const { monsters, search, count } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(search.toLowerCase()));
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
+        <h2>{count}</h2>
+        <button onClick={this.handleClick}>Click Me</button>
         <SearchBox placeholder='Search for Monsters'
           handleChange={this.onSearch} />
         <CardList monsters={filteredMonsters} />
